4c3e85192eadc2dd63ecb4f97e4a9d3bedfd0bf1
Dan Zera <dczera@gmail.com>
2017-10-16 14:04:21 -0500

    handle api errors

--- src/api/modules/errorHandler.js
+++ src/api/modules/errorHandler.js
@@ -1,4 +1,4 @@
 export const apiErrorHandler = (error, req, res, next) => {
-	console.log('apiError', error)
-	res.status(500)
+	console.error(error.stack)
+  res.status(500).send(error.message || error.toString())
 }
