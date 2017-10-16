2d2e0c5d55ba96405e1624ca91877d9d5b5f5945
Dan Zera <dczera@gmail.com>
2017-10-16 11:18:36 -0500

    add comments

--- src/api/resources/playlist/playlist.restRouter.js
+++ src/api/resources/playlist/playlist.restRouter.js
@@ -6,10 +6,10 @@ export const playlistRouter = express.Router()
 playlistRouter.param('id', playlistController.findByParam)
 
 playlistRouter.route('/')
-  .get(playlistController.getAll)
+  .get(playlistController.getAll) // SINCE NO ID IS SPECIFIED IN THE ROUTE --> INDICATES YOU WANT TO GET ALL
   .post(playlistController.createOne)
 
 playlistRouter.route('/:id')
-  .get(playlistController.getOne)
+  .get(playlistController.getOne) // SINCE AN IDE IS SPECIFIED IN THIS ROUTE --> INDICATES YOU WANT TO GET ONE
   .put(playlistController.updateOne)
   .delete(playlistController.deleteOne)
