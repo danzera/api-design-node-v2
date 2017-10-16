ce3feae61350a7d7e78ee756cf2f7cc3c15e342a
Dan Zera <dczera@gmail.com>
2017-10-16 14:35:01 -0500

    fill out song and user models

--- src/api/resources/song/song.model.js
+++ src/api/resources/song/song.model.js
@@ -1,7 +1,37 @@
 import mongoose from 'mongoose'
 
 export const schema = {
-  
+	
+	title: {
+		type: String,
+		required: [true, 'Song needs a title!']
+	},
+	
+	url: {
+		type: String,
+		unique: true,
+		required: [true, 'Song needs a unique URL!']
+	},
+	
+	album: {
+		type: String,
+		required: [true, 'Song needs an album!']
+	},
+	
+	artist: {
+		type: String,
+		required: [true, 'Song needs an artist!']
+	},
+	
+	rating: {
+		type: Number,
+		required: [true, 'Song needs a rating!']
+	},
+
+	favorite: {
+		type: Boolean,
+		required: [true, 'Song needs a type!']
+	}
 }
 
 const songSchema = new mongoose.Schema(schema)
