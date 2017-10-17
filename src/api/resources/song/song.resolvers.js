dab9b84760804c2fd8c55d304a4a95b56e55a152
Dan Zera <dczera@gmail.com>
2017-10-17 14:32:43 -0500

    see solutions

--- src/api/resources/song/song.resolvers.js
+++ src/api/resources/song/song.resolvers.js
@@ -8,9 +8,24 @@ const allSongs = () => {
   return Song.find({}).exec()
 }
 
+const newSong = (_, {input}) => {
+	return Song.create(input)
+}
+
+const updateSong = (_, {input}) => {
+	const {id, ...update} = input
+	// {new: true} <-- returns the new document, after it's updated
+	// gives original document by default
+	return Song.findByIdAndUpdate(id, update, {new: true}).exec()
+}
+
 export const songResolvers = {
   Query: {
     allSongs,
     Song: getSong
+	},
+	Mutation: {
+		newSong,
+		updateSong
   }
 }
